be1b964363ab02e7b5171785f1345b7e8ddfae05
Nate Schultz <ngs32@drexel.edu>
2019-01-24 11:16:20 -0500

    cleaned indents

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def display(train_images, class_names, train_labels):
+    """ display the first 25 images """
     plt.figure(figsize=(10,10))
     for i in range(25):
         plt.subplot(5,5,i+1)
@@ -11,34 +13,64 @@ def display(train_images, class_names, train_labels):
         plt.xlabel(class_names[train_labels[i]])
     plt.show()
 
+def display_single_prediction(predictions, test_labels, test_images, class_names, i=0):
+    """ prediction next to image """
+    plt.figure(figsize=(6,3))
+    plt.subplot(1,2,1)
+    plot_image(i, predictions, test_labels, test_images, class_names)
+    plt.subplot(1,2,2)
+    plot_value_array(i, predictions,  test_labels)
+    plt.show()
 
-def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
+def display_single_prediction2(test_labels,class_names,model,img):
+    """ labeled x axis with other options """
+    img = (np.expand_dims(img,0)) # Add the image to a batch where it's the only member.
+    predictions_single = model.predict(img)
+    plot_value_array(0, predictions_single, test_labels)
+    _ = plt.xticks(range(10), class_names, rotation=45)
+    plt.show()
 
-  plt.imshow(img, cmap=plt.cm.binary)
+def display_multiple_prediction(predictions, test_labels, test_images, class_names, num_rows=5, num_cols=3):
+    """"Plot the first X test images, their predicted label, and the true label
+        Color correct predictions in blue, incorrect predictions in red"""
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions, test_labels, test_images, class_names)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
 
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
+def plot_image(i, predictions_array, true_label, img, class_names):
+    """ helper function to plot an image """
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
                                 100*np.max(predictions_array),
                                 class_names[true_label]),
                                 color=color)
 
 def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
+    """ helper function to plot values """
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
 
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
--- main.py
+++ main.py
@@ -4,28 +4,34 @@ from ml import *
 
 print("Tensorflow version "+tf.__version__)
 
-# import the Fashion MNIST dataset
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+def preprocess(func):
+    def wrapper(*args, **kwargs):
+        # load data
+        ret = func(*args, **kwargs)
+        # scale train/test images to 0-1 values
+        ret[0] = ret[0] / 255.0
+        ret[2] = ret[2] / 255.0
+        return tuple(ret)
+    return wrapper
 
-# preprocess the data: scale to 0-1 values
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+@preprocess
+def load_data():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    return [train_images, train_labels, test_images, test_labels, class_names]
 
-# # display the first 25 images
-# display(train_images, class_names, train_labels)
+if __name__ == "__main__":
 
-model = ml_model(train_images, train_labels)
+    train_images, train_labels, test_images, test_labels, class_names = load_data()
+    model = ml_model(train_images, train_labels)
 
-# evaluate accuracy
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print 'Test accuracy:'+str(test_acc)
 
-# make preductions
-predictions = model.predict(test_images)
-# print "predicted:"
-# print np.argmax(predictions[0])
-# print "actual:"
-# print test_labels[0]
+    predictions = model.predict(test_images)
+
+    display_single_prediction(predictions, test_labels, test_images, class_names, 0)
+    display_single_prediction2(test_labels,class_names,model,test_images[0])
+    display_multiple_prediction(predictions, test_labels, test_images, class_names)
