152727180a8717170765743829ad5eed055b5f0b
Nate Schultz <ngs32@drexel.edu>
2019-01-30 18:37:00 -0500

    read image files, split train/test

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import os
 import keras
 import cv2
+import numpy as np
+import math
+import tensorflow as tf
 
 TRAIN_DATA_PATH = "../training_data"
 
@@ -9,20 +12,20 @@ def preprocess(func):
         # load data
         ret = func(*args, **kwargs)
         # scale train/test images to 0-1 values
-        ret[0] = ret[0] / 255.0
-        ret[2] = ret[2] / 255.0
+        # ret[0] = ret[0] / 255.0
+        # ret[2] = ret[2] / 255.0
         return tuple(ret)
     return wrapper
 
 @preprocess
 def load_data():
-    # images, labels = read_data()
-    # (train_images, train_labels), (test_images, test_labels) = split_data(images, labels)
-    # class_names = list("QWERTYUIOPASDFGHJKLZXCVBNM")
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    images, labels = read_data()
+    (train_images, train_labels), (test_images, test_labels) = split_data(images, labels)
+    class_names = list("QWERTYUIOPASDFGHJKLZXCVBNM")
+    # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+    #                 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    # fashion_mnist = keras.datasets.fashion_mnist
+    # (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
     return [train_images, train_labels, test_images, test_labels, class_names]
 
 def read_data():
@@ -33,15 +36,20 @@ def read_data():
         letter = full_path[len(TRAIN_DATA_PATH)+1:]
         image_path_list = [full_path+"/"+file for file in image_list]
         if len(image_path_list) > 0:
-            ret_images.append(cv2.imread(image_path_list[0]))
-            ret_labels.append(letter)
+            for i in range(len(image_path_list)):
+                image = cv2.imread(image_path_list[i])
+                # https://stackoverflow.com/a/36841746/3799006
+                magic = [0.2989, 0.5870, 0.1140]
+                image = (image[:,:,:3] * magic).sum(axis=2)
+                ret_images.append(image)
+                ret_labels.append(letter)
 
-    return ret_images, ret_labels
+    tf.image.resize_images(ret_images,[28,28])
+    return np.array(ret_images), np.array(ret_labels)
 
-#TODO
 def split_data(images, labels):
-    train_images = images[:-1]
-    train_labels = labels[:-1]
-    test_images = [images[-1]]
-    test_labels = [images[-1]]
+    train_percent = 0.7
+    count = math.floor(len(images)*train_percent)
+    train_images, test_images = images[:count,:], images[count:,:]
+    train_labels, test_labels = labels[:count], labels[count:]
     return (train_images, train_labels), (test_images, test_labels)
