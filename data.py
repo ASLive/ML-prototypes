fb01f8e1b1249dc45c4575ee0cd964381eae9714
Nate Schultz <ngs32@drexel.edu>
2019-01-31 13:16:28 -0500

    got training with our images to barely work

--- data.py
+++ data.py
@@ -1,53 +1,43 @@
 import os
-import keras
-import cv2
 import numpy as np
 import math
-import tensorflow as tf
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
 
 TRAIN_DATA_PATH = "../training_data"
 
-def preprocess(func):
-    def wrapper(*args, **kwargs):
-        # load data
-        ret = func(*args, **kwargs)
-        # scale train/test images to 0-1 values
-        # ret[0] = ret[0] / 255.0
-        # ret[2] = ret[2] / 255.0
-        return tuple(ret)
-    return wrapper
-
-@preprocess
 def load_data():
-    images, labels = read_data()
+    images, labels, class_names = read_data()
     (train_images, train_labels), (test_images, test_labels) = split_data(images, labels)
-    class_names = list("QWERTYUIOPASDFGHJKLZXCVBNM")
-    # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-    #                 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    # fashion_mnist = keras.datasets.fashion_mnist
-    # (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
     return [train_images, train_labels, test_images, test_labels, class_names]
 
 def read_data():
+    """read data from files"""
     ret_images = []
     ret_labels = []
-    for label in list(os.walk(TRAIN_DATA_PATH)):
+    ret_class_names = []
+    float_count = 0.0 # arbitrary map from letter to float
+    for label in list(os.walk(TRAIN_DATA_PATH)): # walk directory
         full_path, image_list = label[0], label[2]
-        letter = full_path[len(TRAIN_DATA_PATH)+1:]
-        image_path_list = [full_path+"/"+file for file in image_list]
-        if len(image_path_list) > 0:
-            for i in range(len(image_path_list)):
-                image = cv2.imread(image_path_list[i])
-                # https://stackoverflow.com/a/36841746/3799006
-                magic = [0.2989, 0.5870, 0.1140]
-                image = (image[:,:,:3] * magic).sum(axis=2)
-                ret_images.append(image)
-                ret_labels.append(letter)
+        letter = full_path[len(TRAIN_DATA_PATH)+1:] # get letter class
+        if len(letter) > 0:
+            # get list of file paths to each image
+            image_path_list = [TRAIN_DATA_PATH+"/"+letter+"/"+file for file in image_list]
+            float_count += 0.1
+            ret_class_names.append(float_count)
+            print(letter, float_count)
+            if len(image_path_list) > 0:
+                # iterate each image
+                for i in range(len(image_path_list)):
+                    # add image, letter to ret array
+                    image = load_img(image_path_list[i])  # this is a PIL image
+                    image = img_to_array(image)
+                    ret_images.append(image)
+                    ret_labels.append(float_count)
 
-    tf.image.resize_images(ret_images,[28,28])
-    return np.array(ret_images), np.array(ret_labels)
+    return np.array(ret_images), np.array(ret_labels), ret_class_names
 
 def split_data(images, labels):
+    """split training and testing data"""
     train_percent = 0.7
     count = math.floor(len(images)*train_percent)
     train_images, test_images = images[:count,:], images[count:,:]
--- main.py
+++ main.py
@@ -18,11 +18,13 @@ if __name__ == "__main__":
     train_images, train_labels, test_images, test_labels, class_names = load_data()
     model = ml_model(train_images, train_labels, retrain_arg, no_save_arg)
 
+    # TODO: fix accuracy
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     print('Test accuracy:'+str(test_acc))
 
     predictions = model.predict(test_images)
 
-    display_single_prediction(predictions, test_labels, test_images, class_names, 0)
-    display_single_prediction2(test_labels,class_names,model,test_images[0])
-    display_multiple_prediction(predictions, test_labels, test_images, class_names)
+    # TODO: fix graphing
+    # display_single_prediction(predictions, test_labels, test_images, class_names, 0)
+    # display_single_prediction2(test_labels,class_names,model,test_images[0])
+    # display_multiple_prediction(predictions, test_labels, test_images, class_names)
