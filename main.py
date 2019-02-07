1d2ebf9c7abfed95dde908515c40d843a6883604
Nate Schultz <ngs32@drexel.edu>
2019-02-07 13:06:45 -0500

    removed todo

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ if __name__ == "__main__":
 
     predictions = model.predict(test_images)
 
-    # TODO: fix graphing
     test_labels = [int(name*10)-1 for name in test_labels] # for graphing
     display_single_prediction(predictions, test_labels, test_images, class_names, 0)
     display_single_prediction2(test_labels,class_names,model,test_images[0])
